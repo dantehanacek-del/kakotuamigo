5f28bb7d45842d36e14bcad1e88f97a50259201d
Dante <dantehanacek@gmail.com>
2026-06-06 20:50:03 -0300

    logo y seo

--- publicar.bat
+++ publicar.bat
@@ -7,12 +7,3 @@ git push
 echo.
 echo Listo! El sitio se actualiza en ~1 minuto en kakotuamigo.vercel.app
 pause
-1 @echo off
-      2 cd /d "C:\Users\dante\Desktop\kakotuamigo"
-      3 git add .
-      4 set /p msg="Descripcion del cambio: "
-      5 git commit -m "%msg%"
-      6 git push
-      7 echo.
-      8 echo Listo! El sitio se actualiza en ~1 minuto en kakotuamigo.vercel.app
-      9 pause
